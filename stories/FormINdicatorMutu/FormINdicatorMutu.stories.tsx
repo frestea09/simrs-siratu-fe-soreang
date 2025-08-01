fcd5bec34227a91e16894d381109948fe3cc91ca
ilman <ilmanfrasetya@gmail.com>
2025-08-01 08:12:15 +0700

    Feat : Add unitest fro Form indicator Mutu
    Add unitest form form indicator mutu

---
+++ stories/FormINdicatorMutu/FormINdicatorMutu.stories.tsx
@@ -0,0 +1,13 @@
+import FormIndicatorMutu from "@/components/organisms/FormIndicatorMutu/FormIndicatorMutu";
+
+export default {
+  title: "Organisms/FormIndicatorMutu",
+  component: FormIndicatorMutu,
+};
+
+const Template = (args: any) => <FormIndicatorMutu {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  // Pass any required args here
+};
